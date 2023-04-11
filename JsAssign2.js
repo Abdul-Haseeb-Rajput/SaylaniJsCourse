40c00a466658197979c04be76c3cafcd07e26b7c
Abdul Haseeb <abdulhaseeb.9820@gmail.com>
2023-04-12 03:38:00 +0500

    Update JsAssign2.js
    Task 2

--- JsAssign2.js
+++ JsAssign2.js
@@ -9,3 +9,24 @@ function addNumberClosure(n) {
 let addFive = addNumberClosure(5);
 console.log(addFive(10)); // Output: 15
 console.log(addFive(20)); // Output: 25
+
+
+// Task 2
+
+function searchArray(arr, val) {
+    //if the array is empty,the value is not found
+    if (arr.length === 0) {
+      return false;
+    }
+    
+    if (arr[0] === val) {
+      return true;
+    } else {
+      return searchArray(arr.slice(1), val);
+    }
+  }
+
+let myArray = [4,2,5,2,3];
+console.log(searchArray(myArray, 3)); // Output: true
+console.log(searchArray(myArray, 6)); // Output: false
+  
